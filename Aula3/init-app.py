c13d7629dfc15e2f18fd250a8ab13d539429c48b
Utilizador do sistema <user@CSI.EngSeg>
2018-02-23 01:18:56 +0000

    init-app done

--- Aula3/init-app.py
+++ Aula3/init-app.py
@@ -2,32 +2,36 @@ import sys
 from eVotUM.Cripto import eccblind
 
 def printUsage():
-    print("Usage: python initSigner-app.py")
+	print("Usage: python initSigner-app.py" )
 
 def parseArgs():
-    if (len(sys.argv) != 2):
-        printUsage()
-    elif (len(sys.argv) == 2):
-		if (argv[1] == "-init"):
-        	init()
+	if (len(sys.argv) != 2 and len(sys.argv) != 1 ):
+		printUsage()
+	elif (len(sys.argv) == 2):
+		if (sys.argv[1] == "-init"):
+			init()
 		else:
-			try:
-				initted = open(argv[1], "r")
-				
-			except IOError:  			
-				print("Error: command unknown")
-		
+			print("Unknown Parameter")
 	elif (len(sys.argv) == 1):
 		normal()
 
+
 def init():
 	initComponents, pRDashComponents = eccblind.initSigner()
-	
+	componentesF = open('xcomponents.data', 'w')
+	componentesF.write(initComponents + "\n")
+	componentesF.write(pRDashComponents)
+	componentesF.close()
 
 def normal():
-    initComponents, pRDashComponents = eccblind.initSigner()
-    print("Output")
-    print("pRDashComponents: %s" % pRDashComponents)
+	#initComponents, pRDashComponents = eccblind.initSigner()
+	try:
+		componentes = open('xcomponents.data', 'r')
+		componentes.readline()
+		pR = componentes.readline()
+		print("pRDashComponents:\n"+pR)
+	except:
+		print("Not initialized")
 
 if __name__ == "__main__":
-    parseArgs()
+	parseArgs()
